45911ac5cc59583cbf7b8fc6bf0386c25670c92f
angitha <angitha.das@cabotsolutions.com>
2017-10-21 21:28:06 +0530

    game tour

--- app/src/main/java/com/example/angitha/mygame/activity/IntroActivity.java
+++ app/src/main/java/com/example/angitha/mygame/activity/IntroActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.FloatRange;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.angitha.mygame.R;
 
@@ -18,7 +19,7 @@ public class IntroActivity extends MaterialIntroActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        enableLastSlideAlphaExitTransition(true);
+        enableLastSlideAlphaExitTransition(false);
 
         getBackButtonTranslationWrapper()
                 .setEnterTranslation(new IViewTranslation() {
@@ -32,21 +33,17 @@ public class IntroActivity extends MaterialIntroActivity {
                         .backgroundColor(R.color.first_slide_background)
                         .buttonsColor(R.color.first_slide_buttons)
                         .image(R.drawable.img_office)
-                        .title("Organize your time with us")
-                        .description("Would you try?")
-                        .build(),
-                new MessageButtonBehaviour(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showMessage("We provide solutions to make you love your work");
-                    }
-                }, "Work with love"));
+                        .title("Brainvita")
+                        .description("A solo strategy game for age group 5" +
+                                " and above and an ideal game for adults too")
+                        .build());
 
         addSlide(new SlideFragmentBuilder()
                 .backgroundColor(R.color.second_slide_background)
                 .buttonsColor(R.color.second_slide_buttons)
-                .title("Want more?")
-                .description("Go on")
+                .title("How To Play?")
+                .description("Start by jumping the grids either horizontally or vertically " +
+                        "over an adjacent grid to an empty hole.Continue until you are left with 1 grid over an adjacent grid to an empty hole.Continue until you are left with 1 grid over an adjacent grid to an empty hole.Continue until you are left with 1 grid over an adjacent grid to an empty hole.Continue until you are left with 1 grid")
                 .build());
 
         addSlide(new CustomSlide());
@@ -71,6 +68,7 @@ public class IntroActivity extends MaterialIntroActivity {
                 .title("That's it")
                 .description("Would you join us?")
                 .build());
+
     }
 
     @Override
--- app/src/main/java/com/example/angitha/mygame/activity/SplashActivity.java
+++ app/src/main/java/com/example/angitha/mygame/activity/SplashActivity.java
@@ -6,6 +6,7 @@ import android.graphics.LinearGradient;
 import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.os.Handler;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -25,10 +26,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
-        TextView title =(TextView) findViewById(R.id.logo);
+        TextView title = (TextView) findViewById(R.id.logo);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "font/pacifico_regular.ttf");
         title.setTypeface(custom_font);
 
@@ -39,19 +40,16 @@ public class SplashActivity extends AppCompatActivity {
 //        title.getPaint().setShader( myShader );
 
         new Handler().postDelayed(new Runnable() {
-
             /*
              * Showing splash screen with a timer. This will be useful when you
              * want to show case your app logo / company
              */
-
             @Override
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
                 Intent i = new Intent(SplashActivity.this, IntroActivity.class);
                 startActivity(i);
-
                 // close this activity
                 finish();
             }
