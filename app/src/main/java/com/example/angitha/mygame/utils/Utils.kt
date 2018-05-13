4891801f2d962b28196f6d7eafa29d6674bef817
angitha.d <angitha.das3d94@gmail.com>
2018-05-13 23:04:17 +0530

    Share App and gradle updated

--- app/src/main/java/com/example/angitha/mygame/utils/Utils.kt
+++ app/src/main/java/com/example/angitha/mygame/utils/Utils.kt
@@ -14,7 +14,7 @@ object Utils {
             val i = Intent(Intent.ACTION_SEND)
             i.type = "text/plain"
             i.putExtra(Intent.EXTRA_SUBJECT, context.resources.getString(R.string.app_name))
-            var sAux = "\nI have crossed level " + GameLevels.getInstance().getHighestLevelCrossed(context) + " in the game Clever Move!" + getEmojiByUnicode(unicodeVictory) + "\nGet it for Android:"
+            var sAux = "I have crossed level " + GameLevels.getInstance().getHighestLevelCrossed(context) + " in the game Clever Move!" + getEmojiByUnicode(unicodeVictory) + "\nGet it for Android:"
             sAux = sAux + "https://play.google.com/store/apps/details?id=" + context.packageName
             i.putExtra(Intent.EXTRA_TEXT, sAux)
             context.startActivity(Intent.createChooser(i, "choose one"))
