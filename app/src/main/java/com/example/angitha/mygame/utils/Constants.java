e58bc81db3d547e21b4aa5fdd233c82188fc2743
angitha.d <angitha.das3d94@gmail.com>
2018-05-21 23:15:52 +0530

    App Update as per user review.Build Check

--- app/src/main/java/com/example/angitha/mygame/utils/Constants.java
+++ app/src/main/java/com/example/angitha/mygame/utils/Constants.java
@@ -8,10 +8,16 @@ public class Constants {
     public static final int SPLASH_TIME_OUT = 1000;
     public static final String KEY_LEVEL = "levelCrossed";
     public static final String MUTE_SOUND = "muteSound";
+
     public static final String ADMOB_APP_ID = "ca-app-pub-6601632087997781~1601504943";
-    public static final String INTERSTITIAL_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";
-    public static final String REWARDED_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
+
+    public static final String INTERSTITIAL_UNIT_ID = "ca-app-pub-6601632087997781/8028781109";
+    public static final String REWARDED_UNIT_ID = "ca-app-pub-6601632087997781/2793476737";
+
     public static final String KEY_UPDATE_REQUIRED = "force_update_required";
     public static final String KEY_CURRENT_VERSION = "force_update_current_version";
     public static final String KEY_UPDATE_URL = "force_update_store_url";
 }
+
+//reward test "ca-app-pub-3940256099942544/5224354917"; //
+//interstitial test "ca-app-pub-3940256099942544/1033173712"; //
