53ba2c0ee0bb227be6f2fc2c95b16e185c50950e
angitha <angitha@qburst.com>
2019-05-10 13:27:14 +0530

    upgrading versions

--- app/src/main/java/com/example/angitha/mygame/utils/Constants.java
+++ app/src/main/java/com/example/angitha/mygame/utils/Constants.java
@@ -9,15 +9,13 @@ public class Constants {
     public static final String KEY_LEVEL = "levelCrossed";
     public static final String MUTE_SOUND = "muteSound";
 
-    public static final String ADMOB_APP_ID = "ca-app-pub-6601632087997781~1601504943";
+    public static final String ADMOB_APP_ID = "";
 
-    public static final String INTERSTITIAL_UNIT_ID = "ca-app-pub-6601632087997781/8028781109";
-    public static final String REWARDED_UNIT_ID = "ca-app-pub-6601632087997781/2793476737";
+    public static final String INTERSTITIAL_UNIT_ID = "";
+    public static final String REWARDED_UNIT_ID = "";
 
     public static final String KEY_UPDATE_REQUIRED = "force_update_required";
     public static final String KEY_CURRENT_VERSION = "force_update_current_version";
     public static final String KEY_UPDATE_URL = "force_update_store_url";
 }
 
-//reward test "ca-app-pub-3940256099942544/5224354917"; //
-//interstitial test "ca-app-pub-3940256099942544/1033173712"; //
